ecb69360f3203b4b48c30e2d122521750536085b
violeine <haluudang@gmail.com>
2022-06-16 17:34:37 +0700

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 import type { NextPage } from "next";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { EpubViewer, ReactEpubViewer } from "react-epub-viewer";
-import { EpubCFI } from "epubjs";
+import ePub, { EpubCFI } from "epubjs";
 const Home: NextPage = () => {
-  const viewerRef = useRef(null);
+  const epub = useRef<HTMLDivElement>(null);
   const p = new EpubCFI();
+
+  useEffect(() => {
+    console.count("run");
+    if (epub.current !== null) {
+      const book = ePub("./alice.epub");
+      var rendition = book.renderTo(epub.current);
+      rendition.display();
+    }
+  }, [epub]);
+
   return (
-    <div style={{ position: "relative", height: "100%" }}>
-      <ReactEpubViewer
-        url={"/alice.epub"}
-        ref={viewerRef}
-        onSelection={(e) => console.log(e)}
-      />
+    <div className="flex justify-between h-screen">
+      <div className="w-1/2" ref={epub}></div>
+      <div className="w-1/2">hello</div>
     </div>
   );
 };
